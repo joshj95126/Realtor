6241892e18daf871527f76e7a123320526564a9b
Joshua Joyce <66971622+joshj95126@users.noreply.github.com>
2020-09-17 10:41:20 -0700

    917

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Realtor
+
+1) make forms - HTML
+2) create event listener - jQuery
+3) take input values from form, pass API to URL - jQuery
+4) display API for Realtor and TripAdvisor - HTML
+5) Style - CSS
+6) Add drop down menu with state codes
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function requestRealtor() {
+function requestRealtor(location, state) {
     var myHeaders = new Headers();
     myHeaders.append("X-RapidAPI-Key", "a08f1d89f5msha8e71f3e2f5cffep1af3f3jsnf24db2d71b5a");
 
@@ -8,15 +8,15 @@ function requestRealtor() {
         redirect: 'follow'
     };
 
-    fetch("https://realtor.p.rapidapi.com/properties/v2/list-for-rent?city=New York City&state_code=NY&limit=200&offset=0", requestOptions)
+    fetch(`https://realtor.p.rapidapi.com/properties/v2/list-for-rent?${location}&state_code=${state}&limit=200&offset=0`, requestOptions)
         .then(response => response.json())
-        .then(result => console.log(result))
+        .then(result => requestRealtor(result))
         .catch(error => console.log('error', error));
 
 
 }
 
-function requestTripAdvisor() {
+function requestTripAdvisor(location) {
     fetch("https://tripadvisor1.p.rapidapi.com/locations/search?location_id=1&limit=30&sort=relevance&offset=0&lang=en_US&currency=USD&units=km&query=New%20York", {
             "method": "GET",
             "headers": {
@@ -24,13 +24,38 @@ function requestTripAdvisor() {
                 "x-rapidapi-key": "5002e774ccmsh38522f8d4d99f31p12b52cjsna087170306fb"
             }
         })
-        .then(response => {
-            console.log(response);
+        .then(res => res.json())
+
+    .then(response => {
+            displayResultsRestaurants(response)
         })
         .catch(err => {
             console.log(err);
         });
 }
 
-requestRealtor()
-requestTripAdvisor()
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        const location = event.target.location.value;
+        const state = event.target.state.value;
+        requestRealtor(location, state)
+        requestTripAdvisor(location + "-" + state)
+    })
+}
+
+$(function() {
+    console.log('App loaded! Waiting for submit!');
+    watchForm();
+});
+
+function displayResultsReal(responseJson) {
+    console.log(responseJson);
+    if (responseJson.status == 'success') {
+
+    }
+}
+
+function displayResultsRestaurants() {
+
+}
