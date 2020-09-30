63bdca14f077a7efd6a855ddabd2409e9a94180f
Joshua Joyce <66971622+joshj95126@users.noreply.github.com>
2020-09-30 08:46:06 -0700

    093020

--- index.js
+++ index.js
@@ -12,21 +12,19 @@ function requestRealtor(location, state) {
         .then(response => response.json())
         .then(result => displayResultsReal(result))
         .catch(error => console.log('error', error));
-
-
 }
 
 function requestTripAdvisor(location) {
     fetch("https://tripadvisor1.p.rapidapi.com/locations/search?location_id=1&limit=20&sort=relevance&offset=0&lang=en_US&type=restaurants&currency=USD&units=km&query=" + location + " USA", {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
-                "x-rapidapi-key": "5002e774ccmsh38522f8d4d99f31p12b52cjsna087170306fb"
-            }
-        })
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
+            "x-rapidapi-key": "5002e774ccmsh38522f8d4d99f31p12b52cjsna087170306fb"
+        }
+    })
         .then(res => res.json())
 
-    .then(response => {
+        .then(response => {
             displayResultsRestaurants(response)
         })
         .catch(err => {
@@ -44,7 +42,7 @@ function watchForm() {
     })
 }
 
-$(function() {
+$(function () {
     console.log('App loaded! Waiting for submit!');
     watchForm();
 });
@@ -54,22 +52,16 @@ function displayResultsReal(responseJson) {
     if (responseJson.properties.length > 0) {
         const html = responseJson.properties.map(item => `
     
-    
-
-
-    
         <div class="realitem">
         
         <img src="${item.photos[0].href}"/>
         <h3>${item.prop_type}
         ${item.address.city}
         ${item.address.line}
-    
-
     </h3>
-    <a href="${item.rdc_web_url}">Link</a>
+    <a target="_new" href="${item.rdc_web_url}">More Info</a>
     </div>`)
-        $('#real_estate').html("<h2>Places For Rent</h2>" + html)
+        $('#real_estate').html("<h2>Places For Rent</h2>" + html.join(""))
     }
 }
 
@@ -84,12 +76,12 @@ function displayResultsRestaurants(responseJson) {
             }
             return `
     
-            <div><h3>${item.result_object.name}
+            <div class="points_interest"><h3>${item.result_object.name}
             
             </h3>
             ${img} 
             </div>`
         })
-        $('#restaurants').html("<h2>Points Of Interest</h2>" + html)
+        $('#restaurants').html("<h2>Points Of Interest</h2>" + html.join(""))
     }
 }
